192461585210037c8085f91dbeed3e9263bb4981
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-09-04 14:58:39 -0500

    Add project files.

---
+++ Alset_Research/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Alset_Research.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
