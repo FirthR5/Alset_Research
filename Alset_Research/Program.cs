389853928bfd7c32c28c12d2699fd69ea3e0c7a4
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-09-04 17:40:38 -0500

    feat: add Auth/Session & DbContext services on program.cs

--- Alset_Research/Program.cs
+++ Alset_Research/Program.cs
@@ -1,7 +1,33 @@
+using Alset_Research.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AlsetJournalsContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DevDB")));
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddAuthentication(
+    CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(
+    option =>
+    {
+        option.LoginPath = "/Home/Login";
+        option.AccessDeniedPath = "/Home/Login";
+        option.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    }
+);
+
 
 var app = builder.Build();
 
