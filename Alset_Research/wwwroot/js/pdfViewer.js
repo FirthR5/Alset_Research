d97c7ceb035094e1f8e84dbc9d4de6e2ff1b41fa
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-09-04 22:44:29 -0500

    refactor(view): remove comments & comments on all Views

--- Alset_Research/wwwroot/js/pdfViewer.js
+++ Alset_Research/wwwroot/js/pdfViewer.js
@@ -1,17 +1,14 @@
 ﻿var pdfjsLib = window['pdfjs-dist/build/pdf'];
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.min.js';
-var scale = 1; // Escala de zoom
-var resolution = 1; // Resolución de PDF
+var scale = 1;
+var resolution = 1;
 
 function LoadPdfFromUrl(url, containerId) {
     try {
-        // Obtener el contenedor correspondiente según el ID del journal
         var pdf_container = document.getElementById("pdf_container_" + containerId);
-        pdf_container.innerHTML = ""; // Limpiar el contenedor previo
+        pdf_container.innerHTML = "";
 
-        // Cargar el PDF
         pdfjsLib.getDocument(url).promise.then(function (pdfDoc) {
-            // Renderizar todas las páginas del PDF
             for (var i = 1; i <= pdfDoc.numPages; i++) {
                 RenderPage(pdfDoc, pdf_container, i);
             }
@@ -24,17 +21,14 @@ function LoadPdfFromUrl(url, containerId) {
 
 function RenderPage(pdfDoc, pdf_container, num) {
     pdfDoc.getPage(num).then(function (page) {
-        // Crear un nuevo elemento canvas
         var canvas = document.createElement('canvas');
         var ctx = canvas.getContext('2d');
         pdf_container.appendChild(canvas);
 
-        // Establecer dimensiones del canvas
         var viewport = page.getViewport({ scale: scale });
         canvas.height = resolution * viewport.height;
         canvas.width = resolution * viewport.width;
 
-        // Renderizar la página del PDF
         var renderContext = {
             canvasContext: ctx,
             viewport: viewport,
