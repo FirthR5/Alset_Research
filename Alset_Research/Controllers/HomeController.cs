a0bd926b9008487d77488252f409eac6419b1a2a
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2024-09-04 22:29:56 -0500

    feat: add all functionality of HomeController
    BREAKING CHANGES:
    Add all functionalities of the system
    - Login
    - Logout
    - List of Researchers
    - Follow Research
    - List of Journals
    - Upload a Journal

--- Alset_Research/Controllers/HomeController.cs
+++ Alset_Research/Controllers/HomeController.cs
@@ -9,6 +9,9 @@ using System.Security.Claims;
 //using Alset_Research.Services;
 using Microsoft.EntityFrameworkCore;
 using Alset_Research.DTO;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Alset_Research.Controllers
 {
@@ -16,25 +19,53 @@ namespace Alset_Research.Controllers
 	{
 		private readonly ILogger<HomeController> _logger;
         private readonly AlsetJournalsContext _context;
+		private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger, AlsetJournalsContext context )
+		public HomeController(ILogger<HomeController> logger, AlsetJournalsContext context, IWebHostEnvironment webHostEnvironment)
 		{
 			_logger = logger;
 			_context = context;
+			_webHostEnvironment = webHostEnvironment;
 		}
 
 		public IActionResult Index()
 		{
-            var my_user = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (!my_user.IsNullOrEmpty()) return View();
-            else return RedirectToAction("Login", "Home");
-		}
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            int id = int.Parse(userId);
+
+            var query = @"
+                    DECLARE @UserId INT = @id;
+                    SELECT U.Id AS ResearcherId, U.FirstName, U.LastName,
+                           CASE 
+                               WHEN F.FollowerId IS NOT NULL THEN 1
+                               ELSE 0
+                           END AS FollowStatus
+                    FROM Users U
+                    LEFT JOIN Followers F ON U.Id = F.ResearcherId AND F.FollowerId = @UserId
+                    WHERE U.Id <> @UserId;
+                ";
+            var userIdParam = new SqlParameter("@id", id);
+
+
+            List<ResearchDTO> result =  _context.ResearchDTOs
+                .FromSqlRaw(query, userIdParam)
+                .ToList();
+
+
+            return View(result);
+        }
         #region login
         public IActionResult Login()
 		{
             var my_user = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (!my_user.IsNullOrEmpty()) return View();
-            else return RedirectToAction("Login", "Home");
+            //if (!my_user.IsNullOrEmpty()) return View();
+            //else return RedirectToAction("Login", "Home");
+            return View();
 		}
 		[HttpPost]
         public async Task<IActionResult> Login(LoginDTO data)
@@ -67,7 +98,6 @@ namespace Alset_Research.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-		[HttpPost]
         public async Task<IActionResult> LogOut()
 
         {
@@ -80,16 +110,92 @@ namespace Alset_Research.Controllers
         #region UploadJournals
         
         public IActionResult UploadJournals() { 
+
 			return View();
 		}
 
 		[HttpPost]
-		public IActionResult UploadJournals(string email) { 
+        [Authorize]
+		public async Task<IActionResult> UploadJournals(UploadJournalDTO journalFile) {
+
+            int userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+            var PdfFileName=await UploadJournal(journalFile.JournalPDF);
+            Journal journal = new Journal()
+            {
+                Title=journalFile.Title,
+                Description=journalFile.Description,
+                PublicationDate=DateOnly.FromDateTime(DateTime.Today),
+                Pdffile = PdfFileName,
+                UserId = userId
+            };
+
+            _context.Journals.Add(journal);
+            await _context.SaveChangesAsync();
+
 			return View();
+		    }
+		private async Task<string> UploadJournal( IFormFile file)
+		{
+            string folderPath = "journals/";
+            string filename = Guid.NewGuid().ToString()  + "_" + file.FileName;
+            folderPath += filename;
+
+			string serverFolder = Path.Combine(_webHostEnvironment.WebRootPath, folderPath);
+
+			await file.CopyToAsync(new FileStream(serverFolder, FileMode.Create));
+
+			return filename;
 		}
 
-        #endregion
+		#endregion
 
+		public IActionResult Journals()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int id = int.Parse(userId);
+
+            var query = @"
+                SELECT J.Id, J.Title, J.Description, J.PublicationDate, J.PDFFile,
+                       U.FirstName AS ResearcherFirstName, U.LastName AS ResearcherLastName
+                FROM Journals J
+                JOIN Followers F ON J.UserId = F.ResearcherId
+                JOIN Users U ON J.UserId = U.Id
+                WHERE F.FollowerId = @id;
+                                    ";
+            var userIdParam = new SqlParameter("@id", id);
+
+            List<JournalDTO> result =  _context.JournalsDTOs
+                .FromSqlRaw(query, userIdParam)
+                .ToList();
+
+
+            return View(result);
+        }
+
+        public async Task<IActionResult> FollowResearch(int IdResearch)
+        {
+			var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId == null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int my_id = int.Parse(userId);
+
+            Follower newFollower = new Follower()
+            {
+                ResearcherId = IdResearch,
+                FollowerId = my_id,
+            };
+
+            _context.Followers.Add(newFollower);
+            _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
